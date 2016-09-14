fe8588781eec57a2392ac62a120a623904a3675a
Hari Prasad <dnshariprasad@gmail.com>
2016-09-14 19:14:22 +0530

    user fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   root 'pages#home'
 
   get 'pages/about'
 
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
